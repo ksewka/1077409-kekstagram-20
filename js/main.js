ad54e4d9ffd9b5481c6a2588d5f82747e511d891
ksewka <54828553+ksewka@users.noreply.github.com>
2020-06-05 15:10:18 +1200

    отрисовка комментов на страницу

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var pictureTemplate = document.querySelector('#picture')
   .querySelector('.picture');
 
 var pictures = document.querySelector('.pictures');
-
+var bigPicture = document.querySelector('.big-picture');
 
 // Функция подбора случайного числа в заданном промежутке
 var getRandomNumber = function (min, max) {
@@ -47,7 +47,7 @@ var createComments = function () {
 var createPost = function (index) {
   var post = {
     url: 'photos/' + (index + 1) + '.jpg', // 1-25
-    description: 'Все ок!',
+    description: 'Все ок!Бери от жизни все!',
     likes: getRandomNumber(15, 200),
     comments: createComments(),
   };
@@ -76,3 +76,42 @@ for (var j = 0; j < postsAmount; j++) {
   fragment.appendChild(getPost(posts[j]));
 }
 pictures.appendChild(fragment);
+
+
+// Заполняем bigPicture информацией из первого элемента массива
+bigPicture.classList.remove('hidden');
+bigPicture.querySelector('.big-picture__img').src = posts[0].url;
+bigPicture.querySelector('.likes-count').textContent = posts[0].likes;
+bigPicture.querySelector('.comments-count').textContent = posts[0].comments.length;
+bigPicture.querySelector('.social__caption').textContent = posts[0].description;
+
+// Функция удаляет комментарии по умолчанию из разметки
+var destroyedComments = function () {
+  var defaultComments = document.querySelectorAll('.social__comment');
+  for (var i = 0; i < defaultComments.length; i++) {
+    defaultComments[i].remove();
+  }
+};
+
+// Функция вставляет сгенерированные комментарии в DOM
+var showComments = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < posts[0].comments.length; i++) {
+    var clonedComment = bigPicture.querySelector('.social__comment').cloneNode(true);
+    clonedComment.querySelector('img').src = posts[0].comments[i].avatar;
+    clonedComment.querySelector('img').alt = posts[0].comments[i].name;
+    clonedComment.querySelector('.social__text').textContent = posts[0].comments[i].message;
+    fragment.appendChild(clonedComment);
+  }
+  destroyedComments(); // Подчищаем комменты с разметки
+  document.querySelector('.social__comments').appendChild(fragment); // Вставляем сгенерированные на страницу
+};
+
+showComments();
+
+// Прячем блоки счётчика комментариев и загрузки новых комментариев
+document.querySelector('.social__comment-count').classList.add('hidden');
+document.querySelector('.comments-loader').classList.add('hidden');
+
+// Чтобы контейнер с фотографиями позади не прокручивался при скролле.
+document.querySelector('body').classList.add('modal-open');
