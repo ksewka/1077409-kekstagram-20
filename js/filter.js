48b017f6a4725a48b029e5f09ed520aef566f132
ksewka <54828553+ksewka@users.noreply.github.com>
2020-06-21 16:05:11 +1200

    реализован слайдер с фильтрами

--- js/filter.js
+++ js/filter.js
@@ -1,18 +1,10 @@
 'use strict';
 
 (function () {
-  var uploadPreview = document.querySelector('.img-upload__preview');
-  var preloadImg = uploadPreview.querySelector('img');
-  var effects = document.querySelectorAll('.effects__radio');
-  var effectsList = document.querySelector('.effects__list');
 
-  effectsList.addEventListener('change', function (evt) {
-    for (var i = 0; i < effects.length; i++) {
-      preloadImg.classList.remove('effects__preview--' + effects[i].value);
-    }
-    if (preloadImg) {
-      preloadImg.classList.add('effects__preview--' + evt.target.value);
-    }
-  });
 
+
+  window.filter = {
+    sliderValue: sliderValue,
+  };
 })();
--- js/slider.js
+++ js/slider.js
@@ -2,34 +2,115 @@
 
 (function () {
   var levelPin = document.querySelector('.effect-level__pin');
-  var liner = document.querySelector('.effect-level__line');
-  var limits = {
-    left: 0,
-    right: liner.offsetWidth,
+  var depthLevel = document.querySelector('.effect-level__depth');
+  var levelValue = document.querySelector('.effect-level__value');
+  var effectLine = document.querySelector('.effect-level__line');
+
+  var effects = document.querySelectorAll('.effects__radio');
+  var effectsList = document.querySelector('.effects__list');
+  var slider = document.querySelector('.effect-level');
+
+  var uploadPreview = document.querySelector('.img-upload__preview');
+  var preloadImg = uploadPreview.querySelector('img');
+  var coefficientMax = 1;
+  var blur = {
+    MIN: 0,
+    MAX: 3
+  };
+  var brightness = {
+    MIN: 1,
+    MAX: 3
+  };
+  var percent = {
+    MIN: 0,
+    MAX: 100
+  };
+  var effectMap = {
+    'none': function () {
+      return '';
+    },
+    'chrome': function (ratio) {
+      return 'grayscale(' + ratio + ')';
+    },
+    'sepia': function (ratio) {
+      return 'sepia(' + ratio + ')';
+    },
+    'marvin': function (ratio) {
+      return 'invert(' + ratio * percent.MAX + '%)';
+    },
+    'phobos': function (ratio) {
+      return 'blur(' + ratio * blur.MAX + 'px)';
+    },
+    'heat': function (ratio) {
+      return 'brightness(' + ratio * brightness.MAX + ')';
+    }
   };
 
+  // Функция для сброса настроек без фильтров
+  var getDefaultSettings = function () {
+    slider.classList.add('hidden');
+    preloadImg.removeAttribute('style');
+    preloadImg.removeAttribute('class');
+    levelValue.setAttribute('value', percent.MIN);
+  };
+  getDefaultSettings();
+
+  // Функция для установки начальных параметров слайда с фильтром
+  var getCustomSettings = function () {
+    slider.classList.remove('hidden');
+    levelPin.style.left = percent.MAX + '%';
+    depthLevel.style.width = percent.MAX + '%';
+    levelValue.setAttribute('value', percent.MAX);
+  };
+
+  // Выбор и применение фильтра
+  effectsList.addEventListener('change', function (evt) {
+    if (evt.target.value === 'none') {
+      getDefaultSettings();
+    } else {
+      for (var i = 0; i < effects.length; i++) {
+        preloadImg.classList.remove('effects__preview--' + effects[i].value);
+      }
+      getCustomSettings();
+      if (preloadImg) {
+        preloadImg.classList.add('effects__preview--' + evt.target.value);
+        preloadImg.style.filter = effectMap[evt.target.value](coefficientMax);
+      }
+    }
+  });
+
+  // Перетаскивание ползунка и изменение эффекта фильтра
   levelPin.addEventListener('mousedown', function (evt) {
-    var startCoords = {
-      x: evt.clientX
+    var startX = evt.clientX;
+    var limits = {
+      left: 0,
+      right: effectLine.getBoundingClientRect().width,
     };
-    console.log(startCoords.x);
+
     var mouseMoveHandler = function (moveEvt) {
-      var shift = {
-        x: startCoords.x - moveEvt.clientX
-      };
-      startCoords = {
-        x: moveEvt.clientX
-      };
-      var levelPinX = levelPin.offseLeft - shift.x;
-      levelPin.style.left = levelPinX + 'px';
+      var shiftX = startX - moveEvt.clientX;
+      startX = moveEvt.clientX;
+      var levelPinX = levelPin.offsetLeft - shiftX;
+      var ratio = levelPinX / limits.right;
+      if (levelPinX >= limits.left && levelPinX <= limits.right) {
+        levelPin.style.left = levelPinX + 'px';
+        depthLevel.style.width = levelPinX + 'px';
+        var levelValueCoefficient = Math.round(ratio * percent.MAX);
+        levelValue.setAttribute('value', levelValueCoefficient);
+        var currentEffect = preloadImg.className.replace(/effects__preview--/, '');
+        preloadImg.style.filter = effectMap[currentEffect](ratio);
+      }
     };
+
     var mouseUpHandler = function () {
-      console.log('up');
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
     };
+
     document.addEventListener('mousemove', mouseMoveHandler);
     document.addEventListener('mouseup', mouseUpHandler);
 
-
   });
+
 })();
 
