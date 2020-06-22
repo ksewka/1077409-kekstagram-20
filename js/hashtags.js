1d72bb40dd432b3e11d09e32e5c707c3de785bfa
ksewka <54828553+ksewka@users.noreply.github.com>
2020-06-22 14:44:37 +1200

    начало валидации хэштегов

---
+++ js/hashtags.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+(function () {
+  var hashtags = document.querySelector('.text__hashtags');
+  hashtags.addEventListener('input', function () {
+    console.log(hashtags.value);
+  });
+
+})();
