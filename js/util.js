3477aaefdf6f317e3641ca71cd8800c805136470
ksewka <54828553+ksewka@users.noreply.github.com>
2020-07-17 21:09:10 +0700

    показ комментов по 5 шт

--- js/util.js
+++ js/util.js
@@ -14,10 +14,25 @@
     }
   };
 
+  //  Сообщение об ошибке
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red; color: white; margin-top: 150px; width: 400px';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+
   window.util = {
     ESC_KEY: 'ESC_KEY',
     ENTER_KEY: 'ENTER_KEY',
     isEscEvent: isEscEvent,
-    isEnterEvent: isEnterEvent
+    isEnterEvent: isEnterEvent,
+    errorHandler: errorHandler
   };
 })();
